94906f6326d75ae4f23678dd05c39b803b6ecfb5
Solomon Himelbloom <2001solomon@gmail.com>
2018-11-23 22:39:03 -0900

    Created Goal Core Data Entity & Attributes

--- goalpost-app/Controller/GoalsVC.swift
+++ goalpost-app/Controller/GoalsVC.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import CoreData
 
 class GoalsVC: UIViewController {
 
@@ -14,7 +15,7 @@ class GoalsVC: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
     }
 
     @IBAction func addGoalBtnWasPressed(_ sender: Any) {
